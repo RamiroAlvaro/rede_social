4ee237d0e5f95da6bccfea994d5b3d4064f23826
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-09-01 20:44:17 -0300

    Extendiendo el modelo de usuario

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR / 'media')
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
